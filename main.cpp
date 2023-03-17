207162c6c8ec354b407f9495541156ff1c20b1d1
Yuanming Chen <yuanming@gmail.com>
2023-03-17 14:30:30 -0600

    Update

--- main.cpp
+++ main.cpp
@@ -1,45 +1,57 @@
 #include <iostream>
 #include <cpprest/http_client.h>
-#include <cpprest/json.h>
-using namespace std;
-using namespace web; // JSON
+#include <cpprest/filestream.h>
+#include <string>
+#include <iostream>
+#include <memory>
+using namespace utility;
+using namespace web;
+using namespace web::http;
 using namespace web::http::client;
+using namespace concurrency::streams;
+
 int main() 
 {
-    http_client client("http://online-live1.services.u-blox.com");
-    pplx::task<web::http::http_response> resp = client.request(
+    auto fileStream = std::make_shared<ostream>();
+    // Open stream to output file.
+	auto requestTask = fstream::open_ostream("users.ubx")
+    .then([=](ostream outFile) {
+        *fileStream = outFile;
+        http_client client("http://online-live1.services.u-blox.com");
+            pplx::task<web::http::http_response> resp = client.request(
 		web::http::methods::GET, U("/GetOnlineData.ashx?token=WthGYmMETUOJEt1tQmnNZg;gnss=gps,glo;datatype=eph"));
-    
-    // attached a handler to be invoked when resp.is_done() is true
-	pplx::task<json::value> jv = resp.then([=]
-	(pplx::task<web::http::http_response> task) {
-		web::http::http_response response = task.get();
+        // attached a handler to be invoked when resp.is_done() is true
+        pplx::task<void> jv = resp.then([=]
+        (pplx::task<web::http::http_response> task) {
+            web::http::http_response response = task.get();
 
-		// Check the status code.
-		if (response.status_code() != 200) {
-			throw std::runtime_error("Returned " + 
-				std::to_string(response.status_code()));
-		}
-		std::cout << "Returned " + 
-			std::to_string(response.status_code())<<endl;
+            // Check the status code.
+            if (response.status_code() != 200) {
+                throw std::runtime_error("Returned " + 
+                    std::to_string(response.status_code()));
+            }
+            std::cout << "Returned " + 
+                std::to_string(response.status_code()) << std::endl;
 
-		// Convert the response body to JSON object.
-		pplx::task<json::value> jsonObject = response.extract_json();
-		return jsonObject;
-	});
+            response.body().read_to_end(fileStream->streambuf()).wait();
+ 
+            // Close the file.
+            return fileStream->close();
+        });
+    });
 
-    // handler to be invoked when json has been extracted
-	jv.then([=](json::value jo){
-		std:: cout<< "Val:" << jo.serialize() << endl;
-	});
+    requestTask.then([=](){
+        std::cout << "Completed!" << std::endl;
+    });
+
 
 	// Wait until json value is ready
 	try {
-		jv.wait();
+		requestTask.wait();
 	}
 	catch (const std::exception & e) {
 		printf("Error exception:%s\n", e.what());
-	}  
-     
+	}
+
     return 0;
 }
